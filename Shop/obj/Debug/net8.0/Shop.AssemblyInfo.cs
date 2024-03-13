55c6937acb54c5b4627452016168803803fe6251
Kenan Ibragimov <102488775+SHIPZz@users.noreply.github.com>
2024-03-13 05:40:30 +0300

    init

--- Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2814a7c1357420c7e9af6e2978dc35fbf67f055a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
